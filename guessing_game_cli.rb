d436643aea9835ea376d870e3abd750eac171d99
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-12 21:31:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,4 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif input != random_number
     puts "Sorry! The computer guessed #{random_number}."
+  elsif input == "exit"
+    puts "Goodbye!"
+  end
 end
